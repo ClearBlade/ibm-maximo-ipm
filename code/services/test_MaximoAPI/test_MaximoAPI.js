0b183de7b5605deb569160d0e748f1da8c4ef62d
Clark Bynum <cbynum@clearblade.com>
2020-08-13 09:46:46 -0500

    cb-dev-kit init and change webpack config to target node; create globalThis library to allow for attaching maximo to global scope so that it can be called from a service; create shell for require library so that we can shim node built-in libraries

--- code/services/test_MaximoAPI/test_MaximoAPI.js
+++ code/services/test_MaximoAPI/test_MaximoAPI.js
@@ -12,22 +12,23 @@
  * @param {CbServer.Resp} resp
  */
 
-function test_MaximoAPI(req,resp){
-    // These are parameters passed into the code service
-    var params = req.params;
-    var options = {
-              protocol: 'http',
-              hostname: 'trial.maximosaas.ibm.com',
-              port: '9080',
-              user: 'asharma@clearblade.com',
-              password: 'asharma',
-              auth_scheme: '/maximo',
-              authtype:'maxauth',
-              islean:0
-          };
+function test_MaximoAPI(req, resp) {
+  // These are parameters passed into the code service
+  var params = req.params;
+  var options = {
+    protocol: "http",
+    hostname: "trial.maximosaas.ibm.com",
+    port: "9080",
+    user: "asharma@clearblade.com",
+    password: "asharma",
+    auth_scheme: "/maximo",
+    authtype: "maxauth",
+    islean: 0,
+  };
+  console.log("before factory");
+  var Maximo = new maximo(options);
+  console.log("after factory");
+  Maximo.authenticate();
 
-    var Maximo = new maximo(options);
-    Maximo.authenticate();
-    (log(http));
-    resp.success("Success");
+  resp.success("Success");
 }
