82b67f3300af401086dab4b59f1752c1eadd7b58
skysharma <sky.sharma@gmail.com>
2021-01-18 17:25:37 -0600

    Set isLean to 1; selecting asset in auth then fcn

--- src/code/services/test_MaximoAPI/test_MaximoAPI.ts
+++ src/code/services/test_MaximoAPI/test_MaximoAPI.ts
@@ -2,28 +2,35 @@ import Maximo from 'ibm-maximo-api';
 
 function test_MaximoAPI(req: CbServer.BasicReq, resp: CbServer.Resp) {
   var options = {
-    protocol: "http",
+    protocol: 'http',
     // ANS: changed hostname, user and password below 1/10/2021
-    //hostname: "trial.maximosaas.ibm.com",
-    hostname: "maximo.clearblade.com",
-    port: "9080",
-    user: "maxadmin",
-    password: "r3g1Ty$@3",
-    auth_scheme: "/maximo",
-    authtype: "maxauth",
-    islean: 0
+    //hostname: 'trial.maximosaas.ibm.com',
+    hostname: 'maximo.clearblade.com',
+    port: '9080',
+    user: 'maxadmin',
+    password: 'r3g1Ty$@3',
+    auth_scheme: '/maximo',
+    authtype: 'maxauth',
+    islean: 1
   };
 
   const maximo = new Maximo(options);
   maximo.authenticate()
-        .then(function(jsessionid)
-        {
-          resp.success(jsessionid);
-        })
-        .fail(function (error)
-        {
-          resp.error(error);
-        });
+    .then(function(jsessionid) {
+      maximo.resourceobject('mxasset')
+      .select(['assetnum', 'serialnum', 'assettype'])
+      .where('assetum')
+      .equal('BU1')
+      .fetch()
+      .then(function(resourceset) {
+        var jsondata = resourceset.thisResourceSet();
+        resp.success(jsondata);
+      })
+    })
+    .fail(function (error)
+    {
+      resp.error(error);
+    });
 }
 
 global.test_MaximoAPI = test_MaximoAPI;
